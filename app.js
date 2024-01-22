25569842024283b04a78cac48272cf20e3ebb862
jsilva314 <silvaj314@yahoo.com>
2024-01-21 19:58:23 -0600

    Now its done

--- app.js
+++ app.js
@@ -15,13 +15,16 @@
 
 //Functional Test shopping cart
 
-// Expect a page asking to log in
-// Expect a wrong password/username message if log in information was incorrect
-// Expect a prompt to register if log in information was not entered
-// Expect the quantity of items in cart to be displayed
-// Expect the total price of items in cart to be displayed
-// Expect the user name to be displayed if logged in
-// Expect an error or message if no items are in shopping cart
-// Expect a an input for payment information
-// Expect a Thank You page once items are submitted and paid for
-// Expect a receipt or confirmation number to be provided once items are paid for
+// When a user clicks checkout, a prompt appears to log in, to register, or to continue
+// When a user creates an account with an invalid e-mail address, an error should be displayed
+// When the wrong password/username is entered, an error indicating wrong password/username entered
+// When the user clicks "log in" but has entered the incorrect login or password, they should receive a message asking to sign up if they havent yet.
+// When a user clicks "logs in" and has filled their information in correctly, they should be taken back to the shopping cart.
+// Once a user is logged in, their username/name should be displayed on the top right of the page
+// When a user adds to cart, the total quantity of items in cart will displayed
+// When a user adds to cart, the total price of items in cart will be displayed
+// When the user clicks "checkout" with no itmes in cart, an error message displays showing no items are in shopping cart
+// When a user clicks "checkout" with items in it, a page opens asking to input payment information
+// When user clicks submit after inputting payment information, a thank you for shopping page displays
+// When a user clicks subit after inputting payment information, a receipt or confirmation number is provided showing details of purchase
+// When a user tries to checkout, a prompt would appear to log in
