a3d677d498d87a0b5b7774d8abe9de916fc573d8
jsilva314 <silvaj314@yahoo.com>
2024-01-21 19:32:58 -0600

    completed code

---
+++ app.js
@@ -0,0 +1,27 @@
+// Unit test Multiplication
+
+// Expect multiplication(5, 7) to be a number
+// Expect multiplication(5, 7) to equal 35
+// Expect multiplication(a, 7) to be an error
+// Expect multiplication(5, 7) to not be a string
+
+// Unit test concatOdds
+
+// Expect concatOdds to use only odd numbers
+// Expect concatOdds to show display numbers from least to greatest
+// Expect concatOdds to not display any even numbers
+// Expect concatOdds to display only numbers
+// Expect concatOdds to display a series of numbers separated by commas
+
+//Functional Test shopping cart
+
+// Expect a page asking to log in
+// Expect a wrong password/username message if log in information was incorrect
+// Expect a prompt to register if log in information was not entered
+// Expect the quantity of items in cart to be displayed
+// Expect the total price of items in cart to be displayed
+// Expect the user name to be displayed if logged in
+// Expect an error or message if no items are in shopping cart
+// Expect a an input for payment information
+// Expect a Thank You page once items are submitted and paid for
+// Expect a receipt or confirmation number to be provided once items are paid for
